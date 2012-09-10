6855bd3947be0924b6f1c8284860309c92d7f2e4
João Xavier <jcxavier@jcxavier.com>
2012-09-10 19:29:21 +0100

    redirected stderr of all rms to /dev/null

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm iarTris.jar 2> /dev/null
-rm `find . -d -name '*.class'`
+rm `find . -d -name '*.class'` 2> /dev/null
 javac IartrisMain.java main/iartris/*.java res/ResClass.java score/iartris/HiScore.java
 jar cvfm iarTris.jar manifest.mf IartrisMain.* iartris.DAT ./main/iartris/ ./res/ ./score/iartris/
